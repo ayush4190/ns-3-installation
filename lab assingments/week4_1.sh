b5c8730bfe53c2aa5f533196af77eaca4d0edee2
mickey4190 <a.ayushkumar1997@gmail.com>
2019-09-25 02:24:15 +0530

    week4 problem 1

---
+++ lab assingments/week4_1.sh
@@ -0,0 +1,5 @@
+./waf --run "tcp-variants-comparison --transport_prot='TcpWestwood' --bandwidth='5Mbps' --tracing=true"
+
+    ##   1.Change transport_prot 
+    ##   3.   Change ns3::TcpSocket::RcvBufSize
+    ##   4.   mtu_bytes
